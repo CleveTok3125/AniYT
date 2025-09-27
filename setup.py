dc08f8f5b9ffd281973bb46147aa47e3cb7b611e
CleveTok3125 <CleveTok3125@gmail.com>
2025-09-27 20:11:19 +0700

    Fix ImportError for ARM/Linux

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import Extension, setup
-import os
 from setuptools.command.build_py import build_py as _build_py
+import os
+import shutil
 import subprocess
 
+SOURCE_ONLY = os.environ.get("SOURCE_ONLY") == "1"
+USE_CYTHON = not SOURCE_ONLY
 
-try:
-    from Cython.Build import cythonize
-    USE_CYTHON = True
-except ImportError:
-    USE_CYTHON = False
+if USE_CYTHON:
+    try:
+        from Cython.Build import cythonize
+    except ImportError:
+        USE_CYTHON = False
+
 
 class build_py(_build_py):
     def run(self):
+
         repo_root = os.path.abspath(os.path.dirname(__file__))
         bin_dir = os.path.join(repo_root, "bin")
         os.makedirs(bin_dir, exist_ok=True)
@@ -20,24 +25,29 @@ class build_py(_build_py):
         output_bin = os.path.join(bin_dir, "ani-tracker")
 
         print(f"Building Go binary at {output_bin} ...")
-        subprocess.check_call(["go", "build", "-o", output_bin, "."], cwd=go_src)
+        if shutil.which("go") is None:
+            print("Skipping Go build, 'go' not found")
+        else:
+            subprocess.check_call(["go", "build", "-o", output_bin, "."], cwd=go_src)
 
         super().run()
 
-extensions = [
-    Extension(
-        name="ani_yt._query",
-        sources=[os.path.join("src", "ani_yt", "_query" + ".pyx" if USE_CYTHON else ".c")],
-    )
-]
-
-
-if USE_CYTHON:
-    extensions = cythonize(extensions, language_level=3)
+ext_modules = []
+ext = Extension(
+    name="ani_yt._query",
+    sources=["src/ani_yt/_query.pyx" if USE_CYTHON else "src/ani_yt/_query.c"]
+)
+if not SOURCE_ONLY:
+    if USE_CYTHON:
+        ext_modules = cythonize([ext], language_level=3)
+    else:
+        ext_modules = [ext]
 
 setup(
     name="AniYT",
-    ext_modules=extensions,
+    ext_modules=ext_modules,
     package_dir={"": "src"},
     cmdclass={"build_py": build_py},
+    setup_requires=["Cython"],
+    zip_safe=False,
 )
