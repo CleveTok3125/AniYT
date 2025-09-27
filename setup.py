a0d10bb3ebb69d273a47b42ff71f233b51dc5e60
CleveTok3125 <CleveTok3125@gmail.com>
2025-09-27 19:33:35 +0700

    Add build go in `setup.py`

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 from setuptools import Extension, setup
 import os
+from setuptools.command.build_py import build_py as _build_py
+import subprocess
+
 
 try:
     from Cython.Build import cythonize
     USE_CYTHON = True
 except ImportError:
     USE_CYTHON = False
+
+class build_py(_build_py):
+    def run(self):
+        repo_root = os.path.abspath(os.path.dirname(__file__))
+        bin_dir = os.path.join(repo_root, "bin")
+        os.makedirs(bin_dir, exist_ok=True)
+
+        go_src = os.path.join(repo_root, "src", "ani_tracker")
+        output_bin = os.path.join(bin_dir, "ani-tracker")
+
+        print(f"Building Go binary at {output_bin} ...")
+        subprocess.check_call(["go", "build", "-o", output_bin, "."], cwd=go_src)
+
+        super().run()
 
 extensions = [
     Extension(
@@ -22,4 +39,5 @@ setup(
     name="AniYT",
     ext_modules=extensions,
     package_dir={"": "src"},
+    cmdclass={"build_py": build_py},
 )
